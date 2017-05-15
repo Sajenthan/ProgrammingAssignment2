e6d85ccf8d236ec5db1d2d52eba564ec36aee7b0
sajenthan <v.sajenthan@gmail.com>
2017-05-15 09:09:03 +0800

    updated R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## function help to store the Matrix inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- solve(y)
+    }
+    get <- function() x
+    setinversion <- function(a) m <<- a
+    getinversion <- function() m
+    list(set = set, get = get,
+         setinversion = setinversion,
+         getinversion = getinversion)
 }
 
 
-## Write a short comment describing this function
+## This function check Matrix inversion if it is not NULL then update the Matrix inversion
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinversion()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinversion(m)
+    m
 }
